5754ae185e72b3ed6cd661eb8f911e5e42b38a40
Krisna31 <81416312+krisna31@users.noreply.github.com>
2023-06-29 13:33:15 +0700

    api done

--- src/utils/api.js
+++ src/utils/api.js
@@ -238,6 +238,60 @@ const api = (() => {
     return vote;
   }
 
+  async function upVoteComment({ threadId, commentId }) {
+    const response = await _fetchWithAuth(`${BASE_URL}/threads/${threadId}/comments/${commentId}/up-vote`, {
+      method: 'POST',
+    });
+
+    const responseJson = await response.json();
+
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    const { data: { vote } } = responseJson;
+
+    return vote;
+  }
+
+  async function downVoteComment({ threadId, commentId }) {
+    const response = await _fetchWithAuth(`${BASE_URL}/threads/${threadId}/comments/${commentId}/down-vote`, {
+      method: 'POST',
+    });
+
+    const responseJson = await response.json();
+
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    const { data: { vote } } = responseJson;
+
+    return vote;
+  }
+
+  async function neutralVoteComment({ threadId, commentId }) {
+    const response = await _fetchWithAuth(`${BASE_URL}/threads/${threadId}/comments/${commentId}/neutral-vote`, {
+      method: 'POST',
+    });
+
+    const responseJson = await response.json();
+
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    const { data: { vote } } = responseJson;
+
+    return vote;
+  }
+  
   return {
     putAccessToken,
     register,
@@ -251,6 +305,9 @@ const api = (() => {
     upVoteThread,
     downVoteThread,
     neutralVoteThread,
+    upVoteComment,
+    downVoteComment,
+    neutralVoteComment,
   };
 })();
 
