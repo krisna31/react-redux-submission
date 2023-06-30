8da034e08f2aa396351826bd50f569ce513cdbca
Krisna31 <81416312+krisna31@users.noreply.github.com>
2023-06-30 10:01:59 +0700

    done

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -59,8 +59,9 @@ function CommentItem({ id, content, createdAt, upVotesBy, downVotesBy, authUser,
           {postedAt(createdAt)}
         </span>
         <span className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400 w-fit gap-2 ">
+          <img className="w-5 h-5 rounded-full" src={owner?.avatar} alt={owner?.name} />
+          {owner?.name}
           <IoIosCreate />
-          {owner.name}
         </span>
       </div>
     </div>
